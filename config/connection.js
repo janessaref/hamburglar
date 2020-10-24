d5066ae75f2fd87e665c0b6aa59c2240e89272f0
Game Over <janessarefong@gmail.com>
2020-10-23 22:05:50 -0700

    add heroku connection in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 const mysql = require("mysql");
+const connection;
 
-// connection to the local mysql
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "jerrifong",
-    password: "mepassword",
-    database: "burgers_db"
-});
-
-// for heroku
-// const connection = mysql.createConnection(process.env.JAWSDB_URL);
+// connection to the local mysql or heroku
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "jerrifong",
+        password: "mepassword",
+        database: "burgers_db"
+    });
+};
 
 // express-handlebars connection
 connection.connect((err) => {
